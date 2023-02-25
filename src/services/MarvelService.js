1acaac87f1179cac984ea49e8bfa7cd2b45d0569
Michael Myers <moek@devmoek.com>
2023-02-25 16:22:23 +0400

    code optimization + random character

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -2,7 +2,6 @@ class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=adc6f74b3e38f537e9917aeffa1b2a90';
 
-
     getResource = async (url) => {
         let res = await fetch(url);
 
@@ -13,13 +12,24 @@ class MarvelService {
         return await res.json()
     }
 
-    getAllCharacters = () => {
-        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
     }
 
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
 
-    getCharacter = (id) => {
-        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.desciption ? `${char.description.slice(0, 210)}...` : 'There is no description for this character yet',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url 
+        }
     }
 }
 
