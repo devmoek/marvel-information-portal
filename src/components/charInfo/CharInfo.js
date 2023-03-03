87ea04da33b9a6272fcb9faceee6f58f474d2545
Michael Myers <moek@devmoek.com>
2023-03-03 15:43:51 +0400

    add prop types

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,9 +1,9 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types'
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import Skeleton from '../skeleton/Skeleton'
 import MarvelService from '../../services/MarvelService';
-
 
 import './charInfo.scss';
 
@@ -128,6 +128,10 @@ const View = ({char}) => {
         </ul>
         </>
     )
+}
+
+CharInfo.propTypes = {
+    charId: PropTypes.number
 }
 
 export default CharInfo;
