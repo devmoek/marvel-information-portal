6b257636e37b69d1ebd4f2828151d61370032464
Michael Myers <moek@devmoek.com>
2023-04-13 12:02:51 +0400

    add react helmet

--- src/components/pages/ComicsPage.js
+++ src/components/pages/ComicsPage.js
@@ -1,9 +1,18 @@
+import {Helmet} from 'react-helmet'
+
 import ComicsList from '../comicsList/ComicsList';
 import AppBanner from '../appBanner/AppBanner';
 
 const ComicsPage = () => {
     return ( 
         <>
+            <Helmet>
+                <meta
+                    name="description"
+                    content="Page about listed comics"
+                />
+                <title>Comics Page</title>
+            </Helmet>
             <AppBanner />
             <ComicsList />
         </>
