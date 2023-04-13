086675625e8ad91728ab607b2083261c0c1b3f65
Michael Myers <moek@devmoek.com>
2023-04-13 12:19:20 +0400

    improvements

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,30 +1,24 @@
-import {lazy, Suspense} from 'react';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AppHeader from "../appHeader/AppHeader";
-import Spinner from "../spinner/Spinner";
-
-const Page404 = lazy(() => import('../pages/404'));
-const MainPage = lazy(() => import('../pages/MainPage'));
-const ComicsPage = lazy(() => import('../pages/ComicsPage'));
-const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
+import {MainPage, ComicsPage, NotFound, SingleComicPage} from '../pages';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
+
     return (
         <Router>
             <div className="app">
                 <AppHeader/>
                 <main>
-                <Suspense fallback={<Spinner/>}>
-                    <Routes>
-                        <Route path = '/' element={<MainPage />} /> 
-                        <Route path = '/comics' element={<ComicsPage />} />
-                        <Route path = '/comics/:comicId' element={<SingleComicPage />} />
-                        <Route path="*" element={<Page404/>} />
-                    </Routes>
-                </Suspense>
+                <Routes>
+                    <Route path = '/' element={<MainPage />} /> 
+                    <Route path = '/comics' element={<ComicsPage />} />
+                    <Route path = '/comics/:comicId' element={<SingleComicPage />} />
+                    <Route path="*" element={<NotFound/>} />
+                </Routes>
                 </main>
             </div>
         </Router>
+
     )
 }
 
--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types'
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
@@ -9,6 +8,7 @@ import useMarvelService from '../../services/MarvelService';
 import './charInfo.scss';
 
 const CharInfo = (props) => {
+
     const [char, setChar] = useState(null);
 
     const {loading, error, getCharacter, clearError} = useMarvelService();
@@ -46,7 +46,7 @@ const CharInfo = (props) => {
     )
 }
 
-const View = ({char, comic}) => {
+const View = ({char}) => {
 
     const {name, description, thumbnail, homepage, wiki, comics} = char;
 
@@ -58,37 +58,37 @@ const View = ({char, comic}) => {
         <>
             <div className="char__basics">
             <img src={thumbnail} alt={name} style={imgStyle}/>
-                <div>
-                    <div className="char__info-name">{name}</div>
-                    <div className="char__btns">
-                        <a href={homepage} className="button button__main">
-                            <div className="inner">homepage</div>
-                        </a>
-                        <a href={wiki} className="button button__secondary">
-                            <div className="inner">Wiki</div>
-                        </a>
-                    </div>
+            <div>
+                <div className="char__info-name">{name}</div>
+                <div className="char__btns">
+                    <a href={homepage} className="button button__main">
+                        <div className="inner">homepage</div>
+                    </a>
+                    <a href={wiki} className="button button__secondary">
+                        <div className="inner">Wiki</div>
+                    </a>
                 </div>
             </div>
-            <div className="char__descr">
-                {description}
-            </div>
-            <div className="char__comics">Comics:</div>
-            <ul className="char__comics-list">
-            {comics.length > 0 ? null : 'There is no comics with this character'}
-                {
-                    comics.map((item, i, comic) => {
+        </div>
+        <div className="char__descr">
+            {description}
+        </div>
+        <div className="char__comics">Comics:</div>
+        <ul className="char__comics-list">
+        {comics.length > 0 ? null : 'There is no comics with this character'}
+            {
+                comics.map((item, i) => {
 
-                        // eslint-disable-next-line
-                        if (i > 9) return;
-                        return(
-                            <li key={i} className="char__comics-item">
-                                <Link to={`/comics/${comic.id}`}>{item.name}</Link>
-                            </li>
-                        )
-                    })
-                }
-            </ul>
+                    // eslint-disable-next-line
+                    if (i > 9) return;
+                    return(
+                        <li key={i} className="char__comics-item">
+                            {item.name}
+                        </li>
+                    )
+                })
+            }
+        </ul>
         </>
     )
 }
